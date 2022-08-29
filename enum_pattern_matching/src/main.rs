44aa5c57e8dc6bb8845efea893e759b0b1b1086e
chorockuin <chorockuin@lginnotek.com>
2022-08-29 16:08:27 +0900

    Add enum & match
    to study
    create enum & match example code
    
    -------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Add : new features
        - Update : feature
        - Remove : feature
        - Refactor : feature's structure not testcase
        - Fix : bug
        - Verify : feature with testcase
    ------------------------------------------------------------------------------|
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

---
+++ enum_pattern_matching/src/main.rs
@@ -0,0 +1,84 @@
+fn main() {
+    test_ip_addr_enum();
+    test_message_enum();
+    test_option();
+    println!("{}", test_match())
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String)
+}
+
+fn test_ip_addr_enum() {
+    let localhost_v4 = IpAddr::V4(127, 0, 0, 1);
+    let localhost_v6 = IpAddr::V6(String::from("::1"));    
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+    }
+}
+
+fn test_message_enum() {
+    let m = Message::Move{x: 32, y: 32};
+    m.call();
+}
+
+fn test_option() {
+    let x = Some(5);
+    match test_option_sub(x) {
+        Some(5) => println!("six"),
+        _ => println!("else")
+    }
+    if let Some(6) = test_option_sub(x) {
+        println!("six");
+    }
+    else {
+        println!("else");
+    }
+}
+
+fn test_option_sub(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
+}
+
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+
+fn test_match() -> u32 {
+    let coin = Coin::Quarter(UsState::Alabama);
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => {
+            println!("Dime!");
+            10
+        },
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+        _ => 0
+    }
+}
