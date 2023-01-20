40ae01da0e6c9743e54baee738a4739612b1cf67
chorockuin <chorockuin@lginnotek.com>
2023-01-20 14:52:34 +0900

    Add webserver
    -------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Add : new features
        - Update : feature
        - Remove : feature
        - Refactor : feature's structure not testcase
        - Fix : bug
        - Verify : feature with testcase
    ------------------------------------------------------------------------------|
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- my_app/src/main.rs
+++ my_app/src/main.rs
@@ -1,23 +1,23 @@
 extern crate my_lib;
 
 fn main() {
-    my_lib::guess_game::sample();               // 2. 추리 게임 튜토리얼
-    my_lib::common::sample();                   // 3. 보편적인 프로그래밍 개념
-    my_lib::ownership::sample();                // 4. 소유권 이해하기
-    my_lib::structures::sample();               // 5. 연관된 데이터들을 구조체로 다루기
-    my_lib::enum_match::sample();               // 6. 열거형과 패턴 매칭
-    my_lib::modules::sample();                  // 7. 모듈
-    my_lib::collections::sample();              // 8. 일반적인 컬렉션
-    my_lib::error::sample();                    // 9. 에러 처리
-    my_lib::generic_traits_lifetime::sample();  // 10. 제너릭 타입, 트레잇, 그리고 라이프타임
-    my_lib::testing::sample();                  // 11. 테스팅
-    my_lib::command_program::sample();          // 12. I/O 프로젝트: 커맨드 라인 프로그램 만들기
-    my_lib::functional::sample();               // 13. 함수형 언어의 특성들: 반복자들과 클로저들
-    my_lib::cargo_crates::sample();             // 14. Cargo와 Crates.io 더 알아보기
-    my_lib::smart_pointer::sample();            // 15. 스마트 포인터
-    my_lib::concurrency::sample();              // 16. 겁없는 동시성
-    my_lib::oop::sample();                      // 17. 러스트의 객체 지향 프로그래밍
-    my_lib::pattern_match::sample();            // 18. 값의 구조와 매칭되는 패턴
-    my_lib::advanced::sample();                 // 19. 고급 기능들
+    // my_lib::guess_game::sample();               // 2. 추리 게임 튜토리얼
+    // my_lib::common::sample();                   // 3. 보편적인 프로그래밍 개념
+    // my_lib::ownership::sample();                // 4. 소유권 이해하기
+    // my_lib::structures::sample();               // 5. 연관된 데이터들을 구조체로 다루기
+    // my_lib::enum_match::sample();               // 6. 열거형과 패턴 매칭
+    // my_lib::modules::sample();                  // 7. 모듈
+    // my_lib::collections::sample();              // 8. 일반적인 컬렉션
+    // my_lib::error::sample();                    // 9. 에러 처리
+    // my_lib::generic_traits_lifetime::sample();  // 10. 제너릭 타입, 트레잇, 그리고 라이프타임
+    // my_lib::testing::sample();                  // 11. 테스팅
+    // my_lib::command_program::sample();          // 12. I/O 프로젝트: 커맨드 라인 프로그램 만들기
+    // my_lib::functional::sample();               // 13. 함수형 언어의 특성들: 반복자들과 클로저들
+    // my_lib::cargo_crates::sample();             // 14. Cargo와 Crates.io 더 알아보기
+    // my_lib::smart_pointer::sample();            // 15. 스마트 포인터
+    // my_lib::concurrency::sample();              // 16. 겁없는 동시성
+    // my_lib::oop::sample();                      // 17. 러스트의 객체 지향 프로그래밍
+    // my_lib::pattern_match::sample();            // 18. 값의 구조와 매칭되는 패턴
+    // my_lib::advanced::sample();                 // 19. 고급 기능들
     my_lib::webserver::sample();                // 20. 마지막 프로젝트: 멀티스레드 웹서버 만들기
 }
--- my_lib/src/webserver/mod.rs
+++ my_lib/src/webserver/mod.rs
@@ -1,2 +1,157 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs::File;
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+// F는 
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) ->
+        Worker {
+
+        let thread = thread::spawn(move ||{
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
+
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+
+                        job.call_box();
+                    },
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+
+                        break;
+                    },
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool {
+            workers,
+            sender,
+        }
+    }
+
+    // F는 한 번만 호출 될 수 있고(FnOnce : 캡쳐한 파라미터의 소유권을 한 번만 가져올 수 있으므로 호출도 한 번만 할 수 있음), 쓰레드 간 소유권을 이동시킬 수 있고(Send), 그리고 전역의 lifetime을 갖음('static)
+    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f); // F 포인터를 생성하고
+        self.sender.send(Message::NewJob(job)).unwrap(); // 
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 pub fn sample() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+     let mut file = File::open(filename).unwrap();
+     let mut contents = String::new();
+
+     file.read_to_string(&mut contents).unwrap();
+
+     let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+     stream.write(response.as_bytes()).unwrap();
+     stream.flush().unwrap();
 }
